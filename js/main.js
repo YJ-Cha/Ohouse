c5e249fceb91a338c34b72cf4a98953ebea16589
YJ-Cha <107171334+YJ-Cha@users.noreply.github.com>
2022-08-04 14:29:10 +0900

    first

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+// section 4 - commercial 광고
+
+var swiper = new Swiper('.swiperCommercial', {
+    // Optional parameters
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+
+    
+
+});
